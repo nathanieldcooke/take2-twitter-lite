d0bb7250df6bf8de2a49ae28f26f136f075c1d64
nathanieldcooke <nathanield.cooke@gmail.com>
2021-09-13 08:43:18 -0700

    tweets migrate, model, and seed

---
+++ db/models/tweet.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Tweet = sequelize.define('Tweet', {
+    message: {
+      type: DataTypes.STRING(280)
+    },
+  }, {});
+  Tweet.associate = function(models) {
+    // associations can be defined here
+  };
+  return Tweet;
+};
