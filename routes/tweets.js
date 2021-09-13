0c95dbf3988c8ba5dae50e45d123ec265e2f0ed2
nathanieldcooke <nathanield.cooke@gmail.com>
2021-09-13 10:09:27 -0700

    complete tweet routes / and /:id

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const { Tweet } = require('../db/models')
+const asyncHandler = require('express-async-handler')
 
 router = express.Router()
 
-router.get("/", (req, res) => {
-    res.json({ message: 'test tweets index' })
-});
+router.get("/", asyncHandler(async (req, res) => {
+    const tweets = await Tweet.findAll()
+    res.json({ tweets });
+}));
+
+router.get("/:id(\\d+)", asyncHandler(async (req, res, next) => {
+    const tweetId = req.params.id;
+    const tweet = await Tweet.findByPk(tweetId);
+    if (tweet) {
+        res.json({ tweet });
+    } else {
+        err = new Error(`tweet with id: ${tweetId}, could not be found`);
+        err.title = 'Tweet not found';
+        err.status = 404;
+        next(err);
+    }
+}));
 
 module.exports = router
